4753ca578082b64e065f24e7692543cacdf9aefb
codeandcodeandcode <codepkcode49@gmail.com>
2022-10-04 07:32:37 +0300

    Go back function added for websites that can go back

--- app/src/main/java/com/example/webviewproject/MainActivity.kt
+++ app/src/main/java/com/example/webviewproject/MainActivity.kt
@@ -15,7 +15,20 @@ class MainActivity : AppCompatActivity() {
         webView.webViewClient = WebViewClient()
 
         webView.loadUrl(" https://adp.fm/episodes/rundown-18-best-of-afrobeats-2019/")
+        // this will enable the javascript settings, it can also allow xss vulnerabilities
         webView.settings.javaScriptEnabled = true
         webView.settings.setSupportZoom(true)
     }
+
+    override fun onBackPressed() {
+            if (webView.canGoBack())
+                webView.goBack()
+                // if your webview cannot go back
+                // it will exit the application
+                else
+            super.onBackPressed()
+
+    }
+
 }
+
